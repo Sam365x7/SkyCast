e0e4fa7943037e4643a9a8fd96b83c8292a078a7
Saami <mrsaamik@gmail.com>
2025-05-01 13:19:19 +0530

    fixed readme file and removed dependency

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'react-native-gesture-handler';
-import 'react-native-reanimated';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import store, {persistor} from './src/redux/store';
